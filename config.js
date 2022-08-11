495a723f5ce7d4cb57d214ccd2fcfcab36cbd778
K-ho344 <huisipingwei@gmail.com>
2022-08-11 13:08:48 +0000

    initial commit

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteTitle: '遊魚菜 平翁',// <title>
+  manifestName: 'Grayscale',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: '平翁',
+  subHeading: 'イカがうまい！',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://www.instagram.com/hiraoimabari/',
+    },
+  ],
+  email: 'anubhav.srivastava00@gmail.com',
+  phone: '000-00000',
+  address: '305 Del Sol, Sunnyvale, CA',
+};
