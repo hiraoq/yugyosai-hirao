cadd40dc229807c7a135504298a66a0a61a0d452
K-ho344 <huisipingwei@gmail.com>
2022-08-12 15:43:14 +0000

    スクロール補助アニメを追加

--- src/components/MastHead.js
+++ src/components/MastHead.js
@@ -6,6 +6,9 @@ export default function MastHead() {
   return (
     <header className="masthead" id="page-top">
       {/* ロゴを追加 */}
+      <div class="scroll">
+        <span class="txt">Scroll</span>
+      </div>
     </header>
   );
 }
